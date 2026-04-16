02d691356e3a279f9ee9fc559133b5c9aa02d825
gbtrueputo <gabrielcostas2005@gmail.com>
2026-04-16 13:56:28 -0300

    trying to fix the connection database error

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -22,6 +22,12 @@ module.exports = {
       }
     },
     pool: {
+      min: 0,
+      max: 5
+    },
+    acquireConnectionTimeout: 10000
+  },
+    pool: {
       min: 2,
       max: 10
     },
@@ -29,4 +35,3 @@ module.exports = {
       tableName: 'knex_migrations'
     }
   }
-}
